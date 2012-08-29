bb162d644df2384dc1c321ed13f518d69e1b9158
Sliim <sliim@mailoo.org>
2012-08-30 00:15:55 +0200

    Add customize for ecb history + keybinding to show/hide ecb windows

--- dotemacs-example.el
+++ dotemacs-example.el
@@ -1,7 +1,11 @@
-;;
-; Setup example for sleemacs
-;;
+(custom-set-faces
+  ;; custom-set-faces was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+ '(default ((t (:inherit nil :stipple nil :foreground "green" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 83 :width normal :foundry "unknown" :family "DejaVu Sans Mono")))))
 
+;; Load sleemacs
 (load-file "~/.emacs.d/sleemacs/init.el")
 
 (custom-set-variables
@@ -13,14 +17,9 @@
  '(ecb-layout-window-sizes (quote (("sliim-layout" (ecb-directories-buffer-name 0.19047619047619047 . 0.9811320754716981) (ecb-methods-buffer-name 0.21164021164021163 . 0.6226415094339622)))))
  '(ecb-options-version "2.40")
  '(ecb-show-sources-in-directories-buffer (quote ("left7" "left13" "left14" "left15" "sliim-layout")))
- '(php-project-list (quote (("WDB-Lib" "~/projects/WDB-Lib/" "" nil "~/projects/WDB-Lib/tests/phpunit.xml" nil (("" . "") "" "" "" "" "" "" "" "") "Wdb" "Wdb") ("WDB-Back" "~/projects/WDB-Back/" "" nil "~/projects/WDB-Back/module/WdbBack/tests/phpunit.xml" nil (("" . "") "" "" "" "" "" "" "" "") "WdbBack" "WdbBack") ("WDB-Front" "~/projects/WDB-Front/" "" nil "~/projects/WDB-Front/module/WdbFront/tests/phpunit.xml" nil (("" . "") "" "" "" "" "" "" "" "") "WdbFront" "WdbFront") ("WDB-Tools" "~/projects/WDB-Tools/" "" nil "" nil (("" . "") "" "" "" "" "" "" "" "") "" "") ("SLiib" "~/projects/SLiib/" "" nil "~/projects/SLiib/tests/phpunit.xml" nil (("" . "") "" "" "" "" "" "" "" "") "SLiib" "SLiib"))))
+ '(ecb-kill-buffer-clears-history (quote auto))
+ '(ecb-history-make-buckets 'never)
  '(phpcs-shell-command "/usr/bin/phpcs")
  '(phpcs-standard "Sliim")
- '(phpunit-shell-command "/usr/bin/phpunit"))
-
-(custom-set-faces
-  ;; custom-set-faces was added by Custom.
-  ;; If you edit it by hand, you could mess it up, so be careful.
-  ;; Your init file should contain only one such instance.
-  ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :stipple nil :foreground "green" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 83 :width normal :foundry "unknown" :family "DejaVu Sans Mono")))))
+ '(phpunit-shell-command "/usr/bin/phpunit")
+ '(etags-select-extended-matches t))
--- init/ecb.el
+++ init/ecb.el
@@ -3,6 +3,7 @@
 (add-to-list 'load-path "~/.emacs.d/sleemacs/plugins/cedet")
 (require 'cedet)
 (require 'ecb)
+
 (setq ecb-tip-of-the-day nil)
 (ecb-activate)
 
@@ -10,4 +11,6 @@
 (global-set-key (kbd "C-c e") 'ecb-goto-window-edit1)
 (global-set-key (kbd "C-c h") 'ecb-goto-window-history)
 (global-set-key (kbd "C-c s") 'ecb-goto-window-sources)
-(global-set-key (kbd "C-c m") 'ecb-goto-window-methods)
+
+(global-set-key (kbd "C-c C-,") 'ecb-show-ecb-windows)
+(global-set-key (kbd "C-c C-;") 'ecb-hide-ecb-windows)
