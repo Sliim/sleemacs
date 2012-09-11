4cfd0e6615a60916de8b07d60e967e5b020591d3
Sliim <sliim@mailoo.org>
2012-09-11 23:00:42 +0200

    Add more messages about project's functions..

--- init/project-utils.el
+++ init/project-utils.el
@@ -15,14 +15,18 @@
       (setq project-tags-file (concat project-emacs-dir "TAGS"))
 
       (when (file-exists-p project-snippets-dir)
+        (message "Loading project's snippets..")
         (add-to-list 'yas/root-directory project-snippets-dir)
         (mapc 'yas/load-directory yas/root-directory))
       (when (file-exists-p project-tags-file)
+        (message "Loading project's tags table..")
         (tags-reset-tags-tables)
         (visit-tags-table project-tags-file))
       (when (file-exists-p (concat project-emacs-dir ".emacs.desktop"))
+        (message "Loading project's desktop..")
         (desktop-read)))
-    (setq current-project project)))
+    (setq current-project project)
+    (message (concat "Project " (php-project-nickname current-project) " opened."))))
 
 ;;; Function that save current desktop in .emacs's project dir
 (defun project-save-desktop ()
@@ -30,7 +34,8 @@
   "Function that save current desktop in .emacs's project dir"
   (if current-project
       (when (file-exists-p (concat (php-project-directory current-project) "/.emacs/"))
-        (desktop-save (concat (php-project-directory current-project) ".emacs/")))
+        (desktop-save (concat (php-project-directory current-project) ".emacs/"))
+        (message "Desktop saved."))
     (message "No project opened..")))
 
 ;;; Function that update project tags
@@ -51,7 +56,8 @@
           --regex-PHP='/interface ([^ ]*)/\1/c/' \
           --regex-PHP='/(public |final |static |abstract |protected |private )+function ([^ (]*)/\2/f/' \
 	      --regex-PHP='/const ([^ ]*)/\1/d/' " (php-project-directory current-project))))
-          (shell-command command)))
+          (shell-command command))
+        (message "Tags table updated."))
     (message "No project opened..")))
 
 ;;; Show the current project
@@ -71,6 +77,7 @@
       (project-save-desktop))
     (desktop-clear)
     (project-desktop-remove-lock-file)
+    (message (concat "Project " (php-project-nickname current-project) " closed."))
     (setq current-project nil)))
 
 ;;; Remove desktop lock file
@@ -80,4 +87,5 @@
   (when current-project
     (setq project-desktop-lock-file (concat (php-project-directory current-project) ".emacs/.emacs.desktop.lock"))
     (when (file-exists-p project-desktop-lock-file)
-      (shell-command (concat "rm " project-desktop-lock-file)))))
+      (shell-command (concat "rm " project-desktop-lock-file))
+      (message "Desktop lock file removed."))))
