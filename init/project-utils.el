bec29f85faec14834c21bcdb187e535deb2807e5
Sliim <sliim@mailoo.org>
2012-09-11 22:46:32 +0200

    Add function to remove project's desktop lock file Remove lock file when close project Clear desktop when close project Ask to save desktop of current project when close project Clean project-open function

--- init/project-utils.el
+++ init/project-utils.el
@@ -7,15 +7,20 @@
   (let ((project (php-project-ask-for-project "Project: ")))
     (project-close-current)
     (php-project-dired-directory project)
-    (when (file-exists-p (concat (php-project-directory project) "/.emacs/"))
-      (cd (concat (php-project-directory project) ".emacs/"))
-      (when (file-exists-p "./snippets/")
-        (add-to-list 'yas/root-directory "./snippets")
+    (setq project-emacs-dir (concat (php-project-directory project) ".emacs/"))
+    (when (file-exists-p project-emacs-dir)
+      (setq desktop-path '(concat (php-project-directory project) ".emacs/"))
+      (setq desktop-dirname project-emacs-dir)
+      (setq project-snippets-dir (concat project-emacs-dir "snippets/"))
+      (setq project-tags-file (concat project-emacs-dir "TAGS"))
+
+      (when (file-exists-p project-snippets-dir)
+        (add-to-list 'yas/root-directory project-snippets-dir)
         (mapc 'yas/load-directory yas/root-directory))
-      (when (file-exists-p "./TAGS")
+      (when (file-exists-p project-tags-file)
         (tags-reset-tags-tables)
-        (visit-tags-table "./TAGS"))
-      (when (file-exists-p "./.emacs.desktop")
+        (visit-tags-table project-tags-file))
+      (when (file-exists-p (concat project-emacs-dir ".emacs.desktop"))
         (desktop-read)))
     (setq current-project project)))
 
@@ -49,6 +54,7 @@
           (shell-command command)))
     (message "No project opened..")))
 
+;;; Show the current project
 (defun project-show-current ()
   (interactive)
   "Show the current project"
@@ -56,10 +62,22 @@
       (message (php-project-nickname current-project))
     (message "none")))
 
+;;; Close current project
 (defun project-close-current ()
   (interactive)
   "Close current project"
   (when current-project
-    (dolist (buffer (buffer-list))
-      (when (eq (php-project-buffer-project (buffer-file-name buffer)) current-project)
-        (kill-buffer buffer)))))
+    (when (y-or-n-p (concat "Save desktop for current project " (php-project-nickname current-project) " "))
+      (project-save-desktop))
+    (desktop-clear)
+    (project-desktop-remove-lock-file)
+    (setq current-project nil)))
+
+;;; Remove desktop lock file
+(defun project-desktop-remove-lock-file ()
+  (interactive)
+  "Remove desktop lock file"
+  (when current-project
+    (setq project-desktop-lock-file (concat (php-project-directory current-project) ".emacs/.emacs.desktop.lock"))
+    (when (file-exists-p project-desktop-lock-file)
+      (shell-command (concat "rm " project-desktop-lock-file)))))
