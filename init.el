30b59869669b177e9df903e991ae173fc7b850b8
Sliim <sliim@mailoo.org>
2012-09-23 22:54:53 +0200

    Add init files comment and gpl disclaimer

--- init.el
+++ init.el
@@ -1,3 +1,28 @@
+;;; init.el ---
+;;
+;;; Author: Sliim <sliim@mailoo.org>
+;;; Description: Sleemacs initializer
+;;
+;; This source file is part of Sleemacs.
+;;
+;; Sleemacs is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+;;
+;; Sleemacs is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License along
+;; with Sleemacs. If not, see <http://www.gnu.org/licenses/gpl-3.0.html>.
+;;
+;;; Commentary:
+;; Add into your ~/.emacs file:
+;; (load-file "~/.emacs.d/sleemacs/init.el")
+;;
+;;; Code:
 (require 'cl)
 
 '(recentf-mode t)
