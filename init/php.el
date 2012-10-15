e9db0edb58b0ec19b104c8498c4aa8efe9d3741d
Sliim <sliim@mailoo.org>
2012-10-15 15:29:13 +0200

    Flymake for php-mode

--- init/php.el
+++ init/php.el
@@ -56,3 +56,21 @@
 (add-hook 'php-mode-hook
           (lambda ()
              (c-set-offset 'case-label '+)))
+
+;; Flymake php
+;; Source: http://sachachua.com/blog/2008/07/emacs-and-php-on-the-fly-syntax-checking-with-flymake/
+(when (require 'flymake nil t)
+  (defun flymake-php-init ()
+    "Use php to check the syntax of the current file."
+    (let* ((temp (flymake-init-create-temp-buffer-copy 'flymake-create-temp-inplace))
+	   (local (file-relative-name temp (file-name-directory buffer-file-name))))
+      (list "php" (list "-f" local "-l"))))
+  
+  (add-to-list 'flymake-err-line-patterns 
+	       '("\\(Parse\\|Fatal\\) error: +\\(.*?\\) in \\(.*?\\) on line \\([0-9]+\\)$" 3 4 nil 2))
+  
+  (add-to-list 'flymake-allowed-file-name-masks '("\\.php$" flymake-php-init))
+
+  (add-hook 'php-mode-hook (lambda () (flymake-mode 1)))
+  (define-key php-mode-map '[M-S-up] 'flymake-goto-prev-error)
+  (define-key php-mode-map '[M-S-down] 'flymake-goto-next-error))
